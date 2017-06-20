b23cb3fb394700cbedaa34953cd75c1410c6118e
So Murota <so@elephantnode.com>
2017-06-20 18:43:35 +0900

    test message!

--- header.php
+++ header.php
@@ -24,7 +24,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 </head>
 
 <body <?php body_class(); ?>>
-
+test!
 <div class="hfeed site" id="page">
 
 	<!-- ******************* The Navbar Area ******************* -->
@@ -49,14 +49,14 @@ $container = get_theme_mod( 'understrap_container_type' );
 						<?php if ( is_front_page() && is_home() ) : ?>
 
 							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-							
+
 						<?php else : ?>
 
 							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						
+
 						<?php endif; ?>
-						
-					
+
+
 					<?php } else {
 						the_custom_logo();
 					} ?><!-- end custom logo -->
